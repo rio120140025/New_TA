1f38b9fa70da36df0890c03d014b284c48d5e856
rio120140025 <100179835+rio120140025@users.noreply.github.com>
2024-03-07 11:55:16 +0700

    fixing minor

--- curanmorpolreslampungutara.sql
+++ curanmorpolreslampungutara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2024 at 05:58 PM
+-- Generation Time: Mar 07, 2024 at 05:54 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
